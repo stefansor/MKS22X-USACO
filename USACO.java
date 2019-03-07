3105b8a1ceab2e454e4c5ffd5a284b2175235099
stefansor <ssorobay00@stuy.edu>
2019-03-07 18:18:03 -0500

    still working on separator3

--- USACO.java
+++ USACO.java
@@ -49,15 +49,43 @@ public class USACO{
     }
   }
 
+  private static void separator3(String fn) throws FileNotFoundException{
+    separator2(fn);
+    File text = new File(fn);
+    Scanner inf = new Scanner(text);
+    //gets ride of lakeorders and grid to get to stomping instructions
+    int waste = 0;
+    for(int i = 0; i < lakeorders[0] * lakeorders[1] + 4; i++){
+      int a = inf.nextInt();
+      waste += a;
+    }
+    //filling in the stomping instructions
+    stompingorders = new int[lakeorders[3]][3];
+    for(int i = 0; i < lakeorders[3]; i++){
+      for(int j = 0; j < 3; j++){
+        stompingorders[i][j] = inf.nextInt();
+      }
+    }
+  }
+
 
 
 
   public static void main(String[] args) throws FileNotFoundException{
-    separator1("makelake1.txt");
-    separator2("makelake1.txt");
+    separator3("makelake1.txt");
     System.out.println(Arrays.toString(lakeorders));
     for(int i = 0; i < land.length; i++){
       System.out.println(Arrays.toString(land[i]));
+    }
+    System.out.println();
+    for(int i = 0; i < newland.length; i++){
+      System.out.println(Arrays.toString(land[i]));
+    }
+    System.out.println();
+
+    System.out.println(stompingorders[0].length);
+    for(int i = 0; i < stompingorders.length; i++){
+      System.out.println(Arrays.toString(stompingorders[i]));
     }
 
     //System.out.println(land[0][0]);
